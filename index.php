75348ee720b7083a4df27cb2b51f72915f4b9b0e
mohamed-Elhossin <m.m.m.elhossin@gmail.com>
2024-09-21 18:35:51 +0300

    hello

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once './shared/navbar.php';
 ?> 
   
 <div class="home">
-    <h1 class="text-center display-1 "> Welcome At Home Page </h1>
+    <h1 class="text-center display-1 "> Welcome At Home Page  Y Bro</h1>
 </div>
 <?php
     include_once './shared/script.php';
